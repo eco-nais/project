c63b4818e7bfb1c5b3b760ef480d77de7d861665
jelly0107 <jelisaveta.letic@gmail.com>
2024-06-27 03:35:37 +0200

    chore: delete Purchase features from TimeseriesDatabaseService

--- TimeseriesDatabaseService/src/main/java/rs/ac/uns/acs/nais/TimeseriesDatabaseService/configuration/InfluxDBConnectionClass.java
+++ TimeseriesDatabaseService/src/main/java/rs/ac/uns/acs/nais/TimeseriesDatabaseService/configuration/InfluxDBConnectionClass.java
@@ -18,7 +18,6 @@ import com.influxdb.query.FluxTable;
 import com.influxdb.client.DeleteApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import rs.ac.uns.acs.nais.TimeseriesDatabaseService.model.Purchase;
 import rs.ac.uns.acs.nais.TimeseriesDatabaseService.model.FixedExpenses;
 
 
@@ -75,106 +74,6 @@ public class InfluxDBConnectionClass {
 
     public void setUrl(String url) {
         this.url = url;
-    }
-
-    public boolean save(InfluxDBClient influxDBClient, Purchase purchase) {
-        boolean flag = false;
-        try {
-            WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
-            purchase.setCreated(Instant.parse("2021-10-12T05:10:15.187484Z"));
-            Point point = Point.measurement("purchases")
-                    .addTag("product_id", purchase.getProduct_id())
-                    .addTag("customer_id", purchase.getCustomer_id())
-                    .addField(purchase.get_field(), (double)purchase.get_value())
-                    .time(purchase.getCreated(), WritePrecision.MS);
-
-            writeApi.writePoint(point);
-            System.out.println("Sacuvao");
-            flag = true;
-        } catch (InfluxException e) {
-            System.out.println("Exception!!" + e.getMessage());
-        }
-        return flag;
-    }
-
-    public boolean writePointbyPOJO(InfluxDBClient influxDBClient, Purchase purchase) {
-        boolean flag = false;
-        try {
-            WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
-
-            purchase.setCreated(Instant.parse("2021-10-12T05:10:15.187484Z"));
-
-            writeApi.writeMeasurement(WritePrecision.MS, purchase);
-            flag = true;
-        } catch (
-
-                InfluxException e) {
-            System.out.println("Exception!!" + e.getMessage());
-        }
-        return flag;
-    }
-
-    public List<Purchase> findAll(InfluxDBClient influxDBClient) {
-        String flux = "from(bucket:\"nais_bucket\") |> range(start:0) |> filter(fn: (r) => r[\"_measurement\"] == \"purchases\") |> sort() |> yield(name: \"sort\")";
-        QueryApi queryApi = influxDBClient.getQueryApi();
-        List<Purchase> purchases = getPurchases(queryApi, flux);
-        return purchases;
-    }
-
-    public List<Purchase> findAllByCustomerId(InfluxDBClient influxDBClient, String customerId) {
-        String fluxForCustomer = "from(bucket:\"nais_bucket\") |> range(start:0) |> filter(fn: (r) => r[\"_measurement\"] == \"purchases\" and r[\"customer_id\"] == \"%s\") |> sort() |> yield(name: \"sort\")";
-        String queryForCustomer = String.format(fluxForCustomer, customerId);
-        QueryApi queryApi = influxDBClient.getQueryApi();
-        List<Purchase> purchases = getPurchases(queryApi, queryForCustomer);
-        return purchases;
-    }
-
-    public List<Purchase> findAllByProductId(InfluxDBClient influxDBClient, String productId) {
-        String fluxForProduct = "from(bucket:\"nais_bucket\") |> range(start:0) |> filter(fn: (r) => r[\"_measurement\"] == \"purchases\" and r[\"product_id\"] == \"%s\") |> sort() |> yield(name: \"sort\")";
-        String queryForProduct = String.format(fluxForProduct, productId);
-        QueryApi queryApi = influxDBClient.getQueryApi();
-        List<Purchase> purchases = getPurchases(queryApi, queryForProduct);
-        return purchases;
-    }
-
-
-    private List<Purchase> getPurchases(QueryApi queryApi, String flux) {
-        List<Purchase> purchases = new ArrayList<>();
-        List<FluxTable> tables = queryApi.query(flux);
-        for (FluxTable fluxTable : tables) {
-            List<FluxRecord> records = fluxTable.getRecords();
-            for (FluxRecord fluxRecord : records) {
-                Purchase purchase = new Purchase();
-                purchase.setCustomer_id((String) fluxRecord.getValueByKey("customer_id"));
-                purchase.setProduct_id((String) fluxRecord.getValueByKey("product_id"));
-                purchase.set_field((String) fluxRecord.getValueByKey("_field"));
-                purchase.set_value((Double) fluxRecord.getValueByKey("_value"));
-                purchase.setCreated((Instant) fluxRecord.getValueByKey("_time"));
-                purchases.add(purchase);
-            }
-        }
-        return purchases;
-    }
-
-    public boolean deleteRecord(InfluxDBClient influxDBClient, String productId) {
-        boolean flag = false;
-        DeleteApi deleteApi = influxDBClient.getDeleteApi();
-
-        try {
-
-            OffsetDateTime start = OffsetDateTime.now().minus(72, ChronoUnit.HOURS);
-            OffsetDateTime stop = OffsetDateTime.now();
-            String predicate = "_measurement=\"purchases\" AND product_id = \"" +
-                    productId +
-                    "\"";
-
-            deleteApi.delete(start, stop, predicate, bucket, org);
-
-            flag = true;
-        } catch (InfluxException ie) {
-            System.out.println("InfluxException: " + ie);
-        }
-        return flag;
     }
 
     // FixedExpenses
